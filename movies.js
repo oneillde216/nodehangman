a389f94762dbf9c66db9dcb9512a384390d99846
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-01-17 17:07:44 -0500

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,5 @@
+exports.movies = {
+
+wordBank: ["Jurassic Park", "Star Wars", "Moneyball", "Patton", "Apocalypse Now", "The Departed", "Office Space"];
+};
+
---
+++ nodehangman.js
@@ -0,0 +1,61 @@
+var Word = require('word.js');
+var prompt = require('prompt');
+
+console.log("Try to guess the Movie");
+
+prompt.start();
+
+
+
+game = {
+  wordBank: ["Jurassic Park", "Star Wars", "Moneyball", "Patton", "Apocalypse Now", "The Departed", "Office Space"],
+  wordsWon: 0,
+  guessesRemaining: 10,
+  currentWord: null,
+  
+  startGame: function (wrd) {
+    this.resetGuesses();
+    this.currentWord = new Word(this.wordBank[Math.floor(Math.random()* this.wordBank.length)]);
+    this.currentWord.getLet();
+    this.promptUser();
+  },
+
+  resetGuesses: function(){
+    this.guessesRemaining = 10;
+  },
+
+  promptUser: function(){
+    var self = this;
+    prompt.get(['guessLetter'], function(err, result){
+      console.log("You guessed: " + result.guessLet);
+      var manyGuessed = self.currentWord.checkLetter(result.guessLet);
+
+      if(manyGuessed ==0) {
+        console.log("WRONG");
+        self.guessesRemaining--;
+        
+      } else {
+        console.log("CORRECT");
+          if(self.currentWord.findWord()){
+            console.log("You won!");
+            return;
+          }
+      }
+
+      console.log("Guesses remaining: " + self.guessesRemaining);
+      if((self.guessesRemaining > 0) && (self.currentWord.found == false)){
+        self.promptUser();
+      }
+      else if(self.guessesRemaining ==0){
+        console.log("Game over. Correct Word is", self.currentWrd.target);
+      } else {
+        console.log(self.currentWrd.wordRender());
+      }
+    });
+
+  }
+
+
+};
+
+game.startGame();
